3a9e2df08dbfaa7c30f211de7afbd963680eb676
YashwanthLakkimsetti <lakkiyashwanth@gmail.com>
2022-10-09 23:35:16 +0530

    Question 19 added in JAVA

---
+++ programs/Question19.java
@@ -0,0 +1,15 @@
+// Java program to illustrate while loop.
+ 
+class Question19 {
+    public static void main(String args[])
+    {
+       
+        int i = 1;
+ 
+        while (i < 6) {
+            System.out.println("Hello World");
+ 
+            i++;
+        }
+    }
+}
