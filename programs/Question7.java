570bb158aeacf2fc5b8c0c6c131c152ce783caa8
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2022-10-09 15:10:58 +0530

    Question7 added to the repo

---
+++ programs/Question7.java
@@ -0,0 +1,15 @@
+public class Question7 {
+    public static void main(String arg[]) {
+        System.out.println("Before swapping");
+        int x = 10;
+        int y = 20;
+        System.out.println("value of x:" + x);
+        System.out.println("value of y:" + y);
+        System.out.println("After swapping");
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("value of x:" + x);
+        System.out.println("value of y:" + y);
+    }
+}
