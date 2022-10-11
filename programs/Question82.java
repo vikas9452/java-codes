98eeb5b34c4ce2691767a05c05089776c116a32b
YashwanthLakkimsetti <lakkiyashwanth@gmail.com>
2022-10-11 16:34:58 +0530

    Added Question82

---
+++ programs/Question82.java
@@ -0,0 +1,16 @@
+public class Question82 {
+ 
+   
+    public static void main(String args[])
+    {
+        
+        String str = "999999999999";
+        System.out.println("String - " + str);
+ 
+        
+        long varLong = Long.parseLong(str);
+ 
+      
+        System.out.println("Long - " + varLong);
+    }
+}
