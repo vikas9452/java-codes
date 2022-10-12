8e35f66c58a9c73d066075e8e468fc24c1f82584
YashwanthLakkimsetti <lakkiyashwanth@gmail.com>
2022-10-12 17:12:32 +0530

    Added Question39

---
+++ programs/Question39.java
@@ -0,0 +1,13 @@
+public class Question39 { 
+  
+    
+    public static void main(String args[]) 
+    { 
+      
+        String str = "geekss@for@geekss"; 
+        String[] arrOfStr = str.split("@", 2); 
+  
+        for (String a : arrOfStr) 
+            System.out.println(a); 
+    } 
+}
