16d156341e05ea68304146d1f08d959d82552246
Atharva Meshram <106589084+im-atharva@users.noreply.github.com>
2022-10-21 14:39:36 +0530

    Create que-4.java

---
+++ programs/que-4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class A{
+    public static void main(String [] args) {
+    int a , b;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value of a: ");
+    a = sc.nextInt();   //Taking input a
+    System.out.println("Enter the value of b: ");
+    b = sc.nextInt();   //Taking input b
+    System.out.println("The value of a is : " + a);
+    System.out.println("The value of b is : " + b);
+}
+}
