b6b5cd1c529029154dc772933db49d82e7af34fa
badalsahani <91333412+badalsahani@users.noreply.github.com>
2022-10-21 11:25:57 +0530

    Create Question41.java

---
+++ programs/Question41.java
@@ -0,0 +1,22 @@
+//How to convert all char in string lower case in java Program
+
+import java.util.*;
+  
+// Main class
+class GFG {
+  
+    // Main driver method
+    public static void main(String args[])
+    {
+  
+        // Custom input string
+        String s = "Geeks for Geeks";
+  
+        // Converting string s to lowercase letter
+        String s2 = s.toLowerCase();
+  
+        // Printing the lowercase string corresponding
+        // to input string
+        System.out.println(s2);
+    }
+}
