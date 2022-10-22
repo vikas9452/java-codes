f97acdea6948400cd9c0e180a0ca5ceedba0c11f
Atharva Meshram <106589084+im-atharva@users.noreply.github.com>
2022-10-22 14:36:43 +0530

    Create swapnumber.java

---
+++ swapnumber.java
@@ -0,0 +1,19 @@
+public class swapNumbers {
+  public static void main (String [] args) {
+
+    int first = 10 , second = 12 , temp;'
+    Systen.out.println("Before swap: ");
+    System.out.println("The value of first number is "+ first ) ;
+    System.out.println("The value of second number is " + second ) ;
+
+    temp = first ;
+    first = second; 
+    second = temp;
+
+    System.out.println("After swap: ");
+    System.out.println("The value of first number is "+ first ) ;
+    System.out.println("The value of second number is " + second ) ;
+
+  }
+
+}
