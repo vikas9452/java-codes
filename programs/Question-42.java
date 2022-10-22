e8c07eaadb5f30dfe46016b5b784288eb7985e20
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-10-22 12:01:54 +0530

    question 42

---
+++ programs/Question-42.java
@@ -0,0 +1,10 @@
+// program for java methods
+public class Main {
+    static void myMethod() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+    }
